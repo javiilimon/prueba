53fa8a82c7321cdf820a50120c4df088098ba734
javiilimon <115158298+javiilimon@users.noreply.github.com>
2022-10-06 14:33:35 +0200

    Add files via upload

---
+++ javie/codigo.js
@@ -0,0 +1,8 @@
+var mascota = "perro";
+var total_mascota = 2;
+var encendido = true;
+
+console.log("Los tipos de datos son");
+console.log(typeof mascota);
+console.log(typeof trabajo);
+console.log(typeof fruta)
